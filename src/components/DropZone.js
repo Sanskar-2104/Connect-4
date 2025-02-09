e2a5f1b372ea23f8f6ba878c195db10317a20e6c
Sanskar-2104 <jainsanskar721@gmail.com>
2025-02-10 00:44:12 +0530

    Added Winner Functionality

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -2,26 +2,67 @@ import { useEffect, useState } from 'react';
 import './DropZone.css';
 import { size,rows,cols } from '../constants/constants';
 import ActiveCoin from './ActiveCoin';
+import Winner from './Winner';
 
 const DropZone = () => {
     const [turn,setTurn] = useState(2)
     const [winner,setWinner] = useState(0)
-    const [dropped,setDropped] = useState([
-        {x: 5, y: 4, player: 1},
-        // {x: 4, y: 1, player: 2},
-        // {x: 3, y: 2, player: 2},
-        // {x: 1, y: 3, player: 1}
-    ])
+    const [dropped,setDropped] = useState([])
 
     const findWinner = () => {
         const p1 = dropped.filter(d => d.player === 1)
         p1.forEach(({x,y}) =>{
-            if(p1.find(m => x === m.x + 1 && y === m.y) &&
+            if (p1.find(m => x === m.x + 1 && y === m.y) &&
                 p1.find(m => x === m.x + 2 && y === m.y) &&
                 p1.find(m => x === m.x + 3 && y === m.y)
             )
             setWinner(1)
+            if (p1.find(m => x === m.x && y === m.y + 1) &&
+                p1.find(m => x === m.x && y === m.y + 2) &&
+                p1.find(m => x === m.x && y === m.y + 3)
+            )
+            setWinner(1)
+            if (p1.find(m => x === m.x + 1 && y === m.y + 1) &&
+                p1.find(m => x === m.x + 2 && y === m.y + 2) &&
+                p1.find(m => x === m.x + 3 && y === m.y + 3)
+            )
+            setWinner(1)
+            if (p1.find(m => x === m.x + 1 && y === m.y - 1) &&
+                p1.find(m => x === m.x + 2 && y === m.y - 2) &&
+                p1.find(m => x === m.x + 3 && y === m.y - 3)
+            )
+            setWinner(1)
         })
+
+        const p2 = dropped.filter(d => d.player === 2)
+        p2.forEach(({x,y}) =>{
+            if (p2.find(m => x === m.x + 1 && y === m.y) &&
+                p2.find(m => x === m.x + 2 && y === m.y) &&
+                p2.find(m => x === m.x + 3 && y === m.y)
+            )
+            setWinner(2)
+            if (p2.find(m => x === m.x && y === m.y + 1) &&
+                p2.find(m => x === m.x && y === m.y + 2) &&
+                p2.find(m => x === m.x && y === m.y + 3)
+            )
+            setWinner(2)
+            if (p2.find(m => x === m.x + 1 && y === m.y + 1) &&
+                p2.find(m => x === m.x + 2 && y === m.y + 2) &&
+                p2.find(m => x === m.x + 3 && y === m.y + 3)
+            )
+            setWinner(2)
+            if (p2.find(m => x === m.x + 1 && y === m.y - 1) &&
+                p2.find(m => x === m.x + 2 && y === m.y - 2) &&
+                p2.find(m => x === m.x + 3 && y === m.y - 3)
+            )
+            setWinner(2)
+        })
+    }
+
+    const reset = () => {
+        setTurn(1)
+        setDropped([])
+        setWinner(0)
     }
 
     useEffect(() => {
@@ -29,6 +70,8 @@ const DropZone = () => {
             setWinner(-1)
         findWinner()
     }, [dropped.length])
+
+    useEffect(() => console.log(winner), [winner])
 
     return <div className='drop-zone'>
         {dropped.map((m,i) =>
@@ -38,12 +81,18 @@ const DropZone = () => {
             />
         )}
 
-        <ActiveCoin
-            turn = {turn}
-            dropped = {dropped}
-            setDropped = {setDropped}
-            setTurn = {setTurn}
-        />
+        {
+            winner 
+            ? <Winner winner = {winner} reset={reset}/>
+            : <ActiveCoin
+                    turn = {turn}
+                    dropped = {dropped}
+                    setDropped = {setDropped}
+                    setTurn = {setTurn}
+                />
+        }
+
+        
     </div>
 }
 
